66f68d3d7936d80cad22de866c488ca2814af577
nribeka <nyoman@openmrs.org>
2013-07-04 21:37:41 -0400

    Update on the UI of listing errors and queue data.

--- omod/src/main/webapp/resources/js/custom/controller.js
+++ omod/src/main/webapp/resources/js/custom/controller.js
@@ -1,7 +1,31 @@
-function QueueCtrl($scope, $location, $data) {
+function ErrorCtrl($scope, $filter, $location, $data) {
+    $data.getErrorList().
+        then(function(response) {
+            $scope.errors = response.data;
+        });
+
+    $scope.selectedErrors = function () {
+        return $filter('filter')($scope.errors, {checked: true});
+    };
+
+    $scope.reQueue = function() {
+        $location.path("/errors");
+    }
 }
 
-function ErrorCtrl($scope, $location, $data) {
+function QueueCtrl($scope, $filter, $location, $data) {
+    $data.getQueueList().
+        then(function(response) {
+            $scope.queues = response.data;
+        });
+
+    $scope.selectedQueues = function () {
+        return $filter('filter')($scope.queues, {checked: true});
+    };
+
+    $scope.deleteQueue = function() {
+        $location.path("/queues");
+    }
 }
 
 function EditSourceCtrl($scope, $location, $data) {
